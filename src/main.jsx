60995b0437ef8d74ecd9a318016f43814ae63ce7
ychanwoo <chxan02@naver.com>
2025-04-13 16:01:20 +0900

    ➕ Add react-router-dom package
    add react-router-dom package

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Signup from './Signup.jsx'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  
-  <Signup />
-)
+  <BrowserRouter>
+    <Signup />
+  </BrowserRouter>
+);
